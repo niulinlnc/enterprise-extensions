2ed787e5b4208cafc12430ae843d56dc8c03f152
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-03-12 10:08:52 -0300

    FIX replace openerp imports for odoo

--- transindar_personalization/product.py
+++ transindar_personalization/product.py
@@ -3,8 +3,8 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from openerp import fields, models, api
-# from openerp.osv import expression
+from odoo import fields, models, api
+# from odoo.osv import expression
 
 
 class ProductTemplate(models.Model):
