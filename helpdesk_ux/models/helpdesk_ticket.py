e10d61dc64ca2127fde04da65144dad01354b9b6
Katherine Zaoral <zaoral@users.noreply.github.com>
2019-06-18 13:35:21 -0300

    [FIX] helpdesk_ux: emails send from stage changes as messages (#130)
    Odoo introduce a new change were all the stage's emails are send like
    notes. But this is causing is trouble because the respond of the
    customer is also taked into account as a note, and not a as message.
    
    In order to avoid this we add some type of patch that return the method
    that do this behaviour as it was working before the new feature.

--- helpdesk_ux/models/helpdesk_ticket.py
+++ helpdesk_ux/models/helpdesk_ticket.py
@@ -19,7 +19,15 @@ class HelpdeskTicket(models.Model):
 
     @api.multi
     def _track_template(self, tracking):
+        # PATCH START: Remove this part after odoo fix the error ...
         res = super(HelpdeskTicket, self)._track_template(tracking)
+        ticket = self[0]
+        changes, tracking_value_ids = tracking[ticket.id]
+        if 'stage_id' in changes and ticket.stage_id.template_id:
+            res['stage_id'] = (ticket.stage_id.template_id, {'auto_delete_message': True})
+        # PATCH END: until this line
+
+        # res = super(HelpdeskTicket, self)._track_template(tracking)
         if self.kanban_state == 'blocked' and self.stage_id.template_id:
             'stage_id' in res and res.pop('stage_id')
         return res
